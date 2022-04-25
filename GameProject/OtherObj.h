e53d29a28610e14f4525c3bbcf57a68273d9360f
Hnanav <vananhtranthihy@gmail.com>
2022-04-25 21:14:46 +0700

    Add OtherObj

---
+++ GameProject/OtherObj.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "CommonFunction.h"
+#include "BaseObj.h"
+
+
+class OtherObj : public BaseObj
+{
+public:
+	OtherObj();
+	~OtherObj();
+	bool LoadImg(string path, SDL_Renderer* renderer, int frameSize);
+	void show(SDL_Renderer* des);
+	void getNum(int _n);
+	void getFrame(int _f);
+	void setclip();
+	void set_renderquad();
+	void getPos(float x, float y);
+	int Get_ObjPosx();
+	int Get_ObjPosy();
+
+private:
+	float xval;
+	float yval;
+	float xpos;
+	float ypos;
+	int widthframe;
+	int heightframe;
+	SDL_Rect FRAME_CLIP[20];
+	int frame;
+	int n ;
+	int frameSize;
+
+};
